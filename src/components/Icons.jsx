f6c2a23ae7e52729acaea5a26b4b21c7f033a17a
Jorge Sosa <jorgesosa@fundaciondevalores.org>
2024-10-04 10:36:50 -0400

    drinks and drinkscategories

---
+++ src/components/Icons.jsx
@@ -0,0 +1,23 @@
+
+
+export function SearchIcon({ className }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`size-9 ${className}`}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+
+    )
+}
+
+
+export function MenuIcon({ className }) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class={`size-9 ${className}`}>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+
+    )
+}
+
+
+export default { MenuIcon, SearchIcon }
